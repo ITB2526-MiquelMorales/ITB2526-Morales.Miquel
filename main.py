4206bcdd016467c2dbdc2779175e24d7dc0ce34f
Miquel <miquel.morales.7e9@itb.cat>
2025-09-30 15:09:04 +0200

    Gambling

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import random
+
 print("Hola Mon :)")
 
+edat=int(input("Quina edat tens?"))
+if edat>=18:
+    print("Ets major d'edat")
+    print("AL CASINO LETS GO")
+    Apuestas=input("roja o negra")
+    resultado=random.choice(["roja","negra"])
+    print("ha salido:" +resultado)
+    if resultado==Apuestas:
+            print("has ganado")
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print ("Programa Finalitzat")
